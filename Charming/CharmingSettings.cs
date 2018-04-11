ac0542110ce16d4736feaac9d3c694dea8da9cd3
kerr1291 <libra.ae@gmail.com>
2018-04-11 07:46:20 -0700

    Created initial repo using empty charming mod as the example starting point.

---
+++ Charming/CharmingSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modding;
+
+namespace CharmingMod
+{
+    public class CharmingSettingsVars
+    {
+        //change when the global settings are updated to force a recreation of the global settings
+        public const string ModVersion = "0.0.1";
+        //change when the global settings are updated to force a recreation of the global settings
+        public const string GlobalSettingsVersion = "0.0.1";
+    }
+
+    //Global (non-player specific) settings
+    public class ModSettings : IModSettings
+    {
+        public void Reset()
+        {
+            BoolValues.Clear();
+            StringValues.Clear();
+            IntValues.Clear();
+            FloatValues.Clear();
+        }
+
+        public string SettingsVersion {
+            get => GetString( "0.0.0" );
+            set {
+                SetString( value );
+            }
+        }       
+    }
+
+    //Player specific settings
+    public class SaveSettings : IModSettings
+    {
+    }
+}
